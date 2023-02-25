84ddc80f2e0000475e2bd9baf275c0d0a74dbb7d
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2023-02-25 13:51:17 +0530

    added config and updated script for build

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+import { addTwoNumber } from "./helper";
 document.write('Hello FrescoPlayers');
+const h1=document.createElement('h1');
+h1.innerText=`WebPack Practice module Bundling: Addtion of 2 number=${addTwoNumber(10, 20)}`;
+document.body.appendChild(h1);
---
+++ src/helper.js
@@ -0,0 +1,4 @@
+function addTwoNumber(n1, n2){
+    return n1+n2;
+}
+module.exports={addTwoNumber}
